2cc7d8eedcb6f3d5d4e25ee038dcd5b3ac5d193f
Nikita <iosifk-2@ya.ru>
2023-04-15 01:03:10 +0300

    домашнее_задание_1_урок

---
+++ HomeWork1/Program.cs
@@ -0,0 +1,77 @@
+﻿Console.Clear();
+/*Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт,
+какое число большее, а какое меньшее.
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3*/
+
+/*Console.WriteLine("Введите первое число:");
+double a = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+double b = double.Parse(Console.ReadLine());
+Console.WriteLine("Больше из двух чисел:");
+if (a > b)
+{
+    double max = a;
+    Console.WriteLine(max);
+}
+if (b > a)
+{
+    double max = b;
+    Console.WriteLine(max);
+}*/
+
+/*Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22*/
+
+/*Console.WriteLine("Введите три числа:");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+Console.WriteLine("Максимальное число:");
+if  (a > b)
+{
+    int max = a;
+    Console.WriteLine(max);
+}
+if (b > c)
+{
+    int max = b;
+    Console.WriteLine(max);
+}
+if (c > a)
+{
+    int max = c;
+    Console.WriteLine(max); 
+}*/
+
+/*Задача 6: Напишите программу, которая на вход принимает число и выдаёт,
+является ли число чётным (делится ли оно на два без остатка).
+4->да
+- 3->нет
+7->нет*/
+
+/*Console.WriteLine("Введите число:");
+int a = int.Parse(Console.ReadLine());
+if(a % 2 == 0)
+{
+    Console.WriteLine("Четное");
+}
+    else
+{
+    Console.WriteLine("Не делится без остатка, нечетное");
+}*/
+
+/*Задача 8: Напишите программу, которая на вход принимает число (N),
+а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8*/
+
+/*Console.WriteLine("Введите число:");
+int a = int.Parse(Console.ReadLine());
+for (int b = 2; b <= a; b+=2)
+{
+    Console.Write(b+" ");
+} */
