198713a07e59af6370df43538bb8a9731bfb584d
Nikita <iosifk-2@ya.ru>
2023-04-16 02:06:24 +0300

    домашнее_задание_2_урок

--- HomeWork1/Program.cs
+++ HomeWork1/Program.cs
@@ -1,11 +1,7 @@
 ﻿Console.Clear();
-/*Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт,
-какое число большее, а какое меньшее.
-a = 5; b = 7 -> max = 7
-a = 2 b = 10 -> max = 10
-a = -9 b = -3 -> max = -3*/
-
-/*Console.WriteLine("Введите первое число:");
+Console.WriteLine("Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт,\r\nкакое число большее, а какое меньшее.\r\na = 5; b = 7 -> max = 7\r\na = 2 b = 10 -> max = 10\r\na = -9 b = -3 -> max = -3");
+ 
+Console.WriteLine("Введите первое число:");
 double a = double.Parse(Console.ReadLine());
 Console.WriteLine("Введите второе число:");
 double b = double.Parse(Console.ReadLine());
@@ -19,59 +15,73 @@ if (b > a)
 {
     double max = b;
     Console.WriteLine(max);
-}*/
+}
+Console.WriteLine("Задача завершена, нажмите что нибудь.");
+Console.ReadKey();
 
-/*Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел
-2, 3, 7 -> 7
-44 5 78 -> 78
-22 3 9 -> 22*/
 
-/*Console.WriteLine("Введите три числа:");
-int a = int.Parse(Console.ReadLine());
-int b = int.Parse(Console.ReadLine());
-int c = int.Parse(Console.ReadLine());
+
+
+
+Console.WriteLine("Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.\r\n    2, 3, 7 -> 7\r\n    44 5 78 -> 78\r\n    22 3 9 -> 22");
+
+Console.WriteLine("Введите три числа:");
+int a1 = int.Parse(Console.ReadLine());
+int b1 = int.Parse(Console.ReadLine());
+int c1 = int.Parse(Console.ReadLine());
 Console.WriteLine("Максимальное число:");
-if  (a > b)
+if  (a1 > b1)
 {
-    int max = a;
+    int max = a1;
     Console.WriteLine(max);
 }
-if (b > c)
+if (b1 > c1)
 {
-    int max = b;
+    int max = b1;
     Console.WriteLine(max);
 }
-if (c > a)
+if (c1 > a1)
 {
-    int max = c;
+    int max = c1;
     Console.WriteLine(max); 
-}*/
+}
 
-/*Задача 6: Напишите программу, которая на вход принимает число и выдаёт,
-является ли число чётным (делится ли оно на два без остатка).
-4->да
-- 3->нет
-7->нет*/
+Console.WriteLine("Задача завершена, нажмите что нибудь.");
+Console.ReadKey();
 
-/*Console.WriteLine("Введите число:");
-int a = int.Parse(Console.ReadLine());
-if(a % 2 == 0)
+
+
+
+
+Console.WriteLine("Задача 6: Напишите программу, которая на вход принимает число и выдаёт,\r\nявляется ли число чётным (делится ли оно на два без остатка).\r\n4->да\r\n- 3->нет\r\n7->нет");
+
+Console.WriteLine("Введите число:");
+int a2 = int.Parse(Console.ReadLine());
+if(a2 % 2 == 0)
 {
     Console.WriteLine("Четное");
 }
     else
 {
     Console.WriteLine("Не делится без остатка, нечетное");
-}*/
+}
 
-/*Задача 8: Напишите программу, которая на вход принимает число (N),
-а на выходе показывает все чётные числа от 1 до N.
-5 -> 2, 4
-8 -> 2, 4, 6, 8*/
+Console.WriteLine("Задача завершена, нажмите что нибудь.");
+Console.ReadKey();
 
-/*Console.WriteLine("Введите число:");
-int a = int.Parse(Console.ReadLine());
-for (int b = 2; b <= a; b+=2)
+
+
+
+
+
+Console.WriteLine("Задача 8: Напишите программу, которая на вход принимает число (N),\r\nа на выходе показывает все чётные числа от 1 до N.\r\n5 -> 2, 4\r\n8 -> 2, 4, 6, 8");
+
+Console.WriteLine("Введите число:");
+int a3 = int.Parse(Console.ReadLine());
+for (int b3 = 2; b3 <= a3; b3+=2)
 {
-    Console.Write(b+" ");
-} */
+    Console.Write(b3+" ");
+}
+
+Console.WriteLine("Задача завершена, нажмите что нибудь.");
+Console.ReadKey();
---
+++ HomeWork2/Program.cs
@@ -0,0 +1,62 @@
+﻿Console.Clear();
+Console.WriteLine("Задача 10: Напишите программу, которая принимает на вход трёхзначное число\r\nи на выходе показывает вторую цифру этого числа.\r\n456 -> 5\r\n782 -> 8\r\n918 -> 1");
+
+Console.WriteLine("Введите трехзначное число:");
+int a = int.Parse(Console.ReadLine());
+int first = a / 10;
+int second = first % 10;
+Console.WriteLine("Вторая цифра чиcла:   " + second);
+
+Console.WriteLine("Задача завершена, нажмите что нибудь.");
+Console.ReadKey();
+
+
+
+
+Console.Clear();
+Console.WriteLine("Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает,\r\nчто третьей цифры нет.\r\n645 -> 5\r\n78 -> третьей цифры нет\r\n32679 -> 6");
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+string numberText = Convert.ToString(number);
+if(numberText.Length > 2)
+{
+    Console.WriteLine("третья цифра ->" + numberText[2]);
+}
+else
+{
+    Console.WriteLine("-> Третьей цифры нет");
+}
+
+Console.WriteLine("Задача завершена, нажмите что нибудь.");
+Console.ReadKey();
+
+
+
+
+
+Console.Clear();
+Console.WriteLine("Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели,\r\nи проверяет, является ли этот день выходным.\r\n6 -> да\r\n7 -> да\r\n1 -> нет");
+
+Console.WriteLine("Введите цифру, обозначающую день недели:");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void day_of_the_Week (int dayNumber)
+{
+    if(dayNumber == 6 || dayNumber == 7)
+    {
+        Console.WriteLine("Этот день выходной");
+    }
+    else if(dayNumber < 1 || dayNumber > 7)
+    {
+        Console.WriteLine("Это не день недели");
+    }
+    else
+    {
+        Console.WriteLine("Этот день не выходной");
+    }
+}
+day_of_the_Week(dayNumber);
+
+Console.WriteLine("Задача завершена, нажмите что нибудь.");
+Console.ReadKey();
