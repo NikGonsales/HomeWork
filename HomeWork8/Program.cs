11fb3963f24a49cf34ca5014acf4a4a9b2545a3b
Nikita <iosifk-2@ya.ru>
2023-06-02 16:54:24 +0300

    домашние задачи: 54,56,58

---
+++ HomeWork8/Program.cs
@@ -0,0 +1,150 @@
+﻿int[,] CreateTwoDimArray(int firstlength, int secondlenght)      // заполняю массив
+{
+    int[,] array = new int[firstlength, secondlenght];
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = new Random().Next(0, 10);
+    return array;
+}
+
+
+void PrintArray(int[,] array)                          // печатаю массив
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j]} ");
+        Console.WriteLine();
+    }
+}
+
+
+void OrderArrayLines(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(0) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+/*Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2*/
+
+
+void Task54()
+{
+    int[,] array = CreateTwoDimArray(3, 4);
+    PrintArray(array);
+    OrderArrayLines(array);
+    Console.WriteLine("Отсортированный массив:");
+    PrintArray(array);
+}
+//Task54();
+
+
+
+void SumLine(int[,]array)
+{
+    int minSumLine = 0;
+    int sumLine = SumLineElements(array, 0);
+    for (int i = 1; i < array.GetLength(0); i++)
+    {
+        int tempSumLine = SumLineElements(array, i);
+        if (sumLine > tempSumLine)
+        {
+            sumLine = tempSumLine;
+            minSumLine = i;
+        }
+    }
+    Console.WriteLine($"\n{minSumLine + 1} - строкa с наименьшей суммой ({sumLine}) элементов ");
+}
+
+
+int SumLineElements(int[,] array, int i)
+{
+    int sumLine = array[i, 0];
+    for (int j = 1; j < array.GetLength(1); j++)
+    {
+        sumLine += array[i, j];
+    }
+    return sumLine;
+}
+
+/*Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка*/
+
+void Task56()
+{
+    int[,] array = CreateTwoDimArray(3, 4);
+    PrintArray(array);
+    SumLine(array);
+}
+//Task56();
+
+
+/*Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18*/
+
+void MultiplyMatrix(int[,] firstMartrix, int[,] secondMartrix, int[,] resultMatrix)
+{
+    for (int i = 0; i < resultMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < resultMatrix.GetLength(1); j++)
+        {
+            int sum = 0;
+            for (int k = 0; k < firstMartrix.GetLength(1); k++)
+            {
+                sum += firstMartrix[i, k] * secondMartrix[k, j];
+            }
+            resultMatrix[i, j] = sum;
+        }
+    }
+}
+
+
+void Task58()
+{
+    int[,] firstMartrix = CreateTwoDimArray(2, 2);
+    Console.WriteLine($"\nПервая матрица:");
+    PrintArray(firstMartrix);
+    int[,] secondMartrix = CreateTwoDimArray(2, 2);
+    Console.WriteLine($"\nВторая матрица:");
+    PrintArray(secondMartrix);
+
+    int[,] resultMatrix = CreateTwoDimArray(2, 2);
+
+    MultiplyMatrix(firstMartrix, secondMartrix, resultMatrix);
+    Console.WriteLine($"\nПроизведение первой и второй матриц:");
+    PrintArray(resultMatrix);
+
+}
+
+// Task58();
