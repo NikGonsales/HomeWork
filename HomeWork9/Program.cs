0ad9b1cfc4cf71fffc0f8a5263e2bb1c5a2eed09
Nikita <iosifk-2@ya.ru>
2023-06-02 16:55:12 +0300

    домашние задачи: 64,66,68

---
+++ HomeWork9/Program.cs
@@ -0,0 +1,46 @@
+﻿/*Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"*/
+
+void PrintNum(int n)
+{
+    Console.WriteLine(n);
+    if (n != 1)
+    {
+        PrintNum(n - 1);
+    }
+}
+//PrintNum(8);
+
+
+/*Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15-> 120
+M = 4; N = 8. -> 30*/
+
+
+int SumNum(int first_numbers, int second_numbers)
+{
+    if (first_numbers != second_numbers)
+    {
+        return first_numbers + SumNum(first_numbers + 1, second_numbers);
+    }
+    return first_numbers;
+}
+//Console.WriteLine(SumNum(4, 8));
+
+
+/*Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3->A(m, n) = 9
+m = 3, n = 2->A(m, n) = 29*/
+
+int A(int n, int m)
+{
+    if (n == 0)
+        return m + 1;
+    else
+      if ((n != 0) && (m == 0))
+        return A(n - 1, 1);
+    else
+        return A(n - 1, A(n, m - 1));
+}
+//Console.WriteLine(A(3, 2));
